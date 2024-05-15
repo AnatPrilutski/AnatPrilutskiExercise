4146ab0665dafe6eed05bd402d69aab752c92857
Anat Prilutski <anat.prilutski@gmail.com>
2024-05-15 21:45:50 +0300

    Added for part3- Node and startes linkedList

---
+++ Part3/Part3/Part3/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3
+{
+    public class Node
+    {
+        private int Value { get; set; }
+        private Node Next { get; set; }
+
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+
+        public Node(int value, Node next) 
+        { 
+            this.Value = value;
+            this.Next = next;
+        }
+
+    }
+}
